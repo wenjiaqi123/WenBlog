a03009475994ec8d0058d7c2fbb32a1795e7239d
wen7 <939949243@qq.com>
2024-11-19 15:11:00 +0800

    导航栏 nav

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,36 +9,36 @@ import {
 } from "./chunk-LW4I4DCF.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "E:/VitePress/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default10 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default11 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default12 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default13 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default14 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default15 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default16 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default17 } from "E:/VitePress/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default10 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default11 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default12 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default13 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default14 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default15 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default16 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default17 } from "F:/xj/WenBlog/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -15,18 +15,75 @@ export default defineConfig({
             {text: '首页', link: '/'},
             {
                 text: '前端', items: [
-                    {text: 'Web', link: '/Html'},
-                    {text: 'App', link: '/markdown-examples'},
-                    {text: 'MiniApp', link: '/markdown-examples'},
-                    {text: 'Client', link:"/markdown-examples"},
+                    {text: 'HTML', link: '/Html'},
+                    {text: 'CSS', link: '/Html'},
+                    {text: 'JavaScript', link: '/Html'},
+                    {text: 'Vue3', link: '/Html'},
+                    {text: '小程序 Wechat', link: '/markdown-examples'},
+                    {text: '客户端 QT', link:"/markdown-examples"},
                 ]
             },
-            {text: 'Java', link: '/Java'},
-            {text: 'Python', link: '/Python'},
+            {text: 'Java', items: [
+                    {text: '基础', link: '/Html'},
+                    {text: '并发编程', link: '/Html'},
+                    {text: 'Maven', link: '/Html'},
+                    {text: 'SpringBoot', link: '/markdown-examples'},
+                    {text: 'SpringMVC', link: '/markdown-examples'},
+                    {text: 'Mybatis', link: '/markdown-examples'},
+                    {text: 'SpringDataJPA', link:"/markdown-examples"},
+                    {text: 'SpringCloudAlibaba', link: '/markdown-examples'},
+                ]},
+            {text: 'Python', items: [
+                    {text: '基础语法', link: '/Html'},
+                    {text: 'Numpy', link: '/Html'},
+                    {text: 'Scipy', link: '/markdown-examples'},
+                    {text: 'Pandas', link: '/markdown-examples'},
+                    {text: 'Matplotlib', link:"/markdown-examples"},
+                ]},
             {text: 'Golang', link: '/Golang'},
-            {text: 'DataBase', link: '/DataBase'},
-            {text: 'Operations', link: '/markdown-examples'},
-            {text: 'Projects', link: '/markdown-examples'},
+            {text: '数据库',  items: [
+                    {text: 'MySQL', link: '/Html'},
+                    {text: 'Redis', link: '/markdown-examples'},
+                    {text: 'Postgresql', link: '/Html'},
+                    {text: 'ElasticSearch', link: '/markdown-examples'},
+                    {text: 'MongoDB', link: '/markdown-examples'},
+                    {text: 'Neo4j', link: '/markdown-examples'},
+                    {text: 'Milvus', link: '/markdown-examples'},
+                ]},
+            {text: '运维',  items: [
+                    {text: 'Linux', link: '/Html'},
+                    {text: 'Shell', link: '/Html'},
+                    {text: 'Docker', link: '/Html'},
+                    {text: 'Jenkins', link: '/Html'},
+                    {text: 'Kubernetes', link: '/markdown-examples'},
+                ]},
+            {text: '组件',  items: [
+                    {text: 'Nginx', link: '/Html'},
+                    {text: 'MinIO', link: '/Html'},
+                    {text: 'Nacos', link: '/Html'},
+                    {text: 'RabbitMQ', link: '/markdown-examples'},
+                    {text: 'RocketMQ', link: '/markdown-examples'},
+                ]},
+            {text: '大数据',  items: [
+                    {text: 'HDFS', link: '/Html'},
+                    {text: 'Yarn', link: '/Html'},
+                    {text: 'MapReduce', link: '/Html'},
+                ]},
+            {text: '项目', items: [
+                    {text: 'BaoBaoQiMing', link: '/Html'},
+                    {text: 'ZhaoGuiLv', link: '/Html'},
+                    {text: 'Admin', link: '/Html'},
+                ]},
+            {text: '其他', items: [
+                    {text: 'Git', link: '/Html'},
+                    {text: 'JetBrains', link: '/Html'},
+                    {text: '设计模式', link: '/Html'},
+                    {text: '数据结构与算法', link: '/Html'},
+                    {text: '网络', link: '/markdown-examples'},
+                    {text: '架构', link: '/Html'},
+                    {text: 'VitePress', link: '/Html'},
+                    {text: '其他', link: '/Html'},
+                ]},
         ],
 
         sidebar:{
@@ -40,9 +97,10 @@ export default defineConfig({
         },
 
         socialLinks: [
-            {icon: 'wechat', link: 'https://github.com/vuejs/vitepress'},
-            {icon: 'github', link: 'https://github.com/vuejs/vitepress'},
-            {icon: 'gitee', link: 'https://github.com/vuejs/vitepress'},
+            {icon: 'wechat', link: '/social_wechat'},
+            {icon: 'github', link: 'https://github.com/wenjiaqi123'},
+            {icon: 'gitee', link: 'https://gitee.com/wjq303812'},
+            {icon: 'csdn', link: 'https://blog.csdn.net/Wen_J_Q'}
         ],
 
         // returnToTopLabel: "返回顶部",
